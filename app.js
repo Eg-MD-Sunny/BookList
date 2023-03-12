577a1f80a3a9cf530be8c666f49e97505786b3a1
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-03-13 01:17:19 +0600

    Modify

--- app.js
+++ app.js
@@ -14,5 +14,29 @@ const bookList = document.querySelector("#book-list");
 
 btn.addEventListener("click",function(e){
     e.preventDefault();
-    console.log("Hello");
+
+    if(title.value == '' && author.value == '' && year.value == ''){
+        alert('Please Valid Input');
+    }else{
+        const newRow = document.createElement('tr');
+        // console.log(newRow);
+
+        //Creating New Title
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML = title.value;
+        console.log(newTitle);
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML = author.value;
+        console.log(newAuthor);
+        newRow.appendChild(newAuthor);
+
+        const newYear = document.createElement('th');
+        newYear.innerHTML = year.value;
+        console.log(newYear);
+        newRow.appendChild(newYear);
+
+        bookList.appendChild(newRow);
+    }
 })
